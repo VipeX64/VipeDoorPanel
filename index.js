7dcbe3b46b39e6963569ee0ebba0477558ac0d5c
VipeX64 <netopyr.64@seznam.cz>
2023-04-19 16:55:39 +0200

    please work

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+// const app = require('express')();
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
+// const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', (socket) => {
+//   socket.on('send message', msg => {
+//     io.emit('send message', msg);
+//   });
+
+// });
+
+// http.listen(port, () => {
+//   console.log(`Socket.IO server running at http://localhost:${port}/`);
+// });
+
+
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 8080;
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  console.log('user connected');
   socket.on('send message', msg => {
     io.emit('send message', msg);
   });
-
 });
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+server.listen(port, function () {
+  console.log(`Listening on port ${port}`);
 });
