737267b5e9a5b0aba5aac0c029ab82c18dfe9b01
VipeX64 <netopyr.64@seznam.cz>
2023-04-20 06:43:47 +0200

    test no socket.io

--- index.js
+++ index.js
@@ -17,22 +17,24 @@
 // http.listen(port, () => {
 //   console.log(`Socket.IO server running at http://localhost:${port}/`);
 // });
-
-
 const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 const port = process.env.PORT || 3001;
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.on('send message', msg => {
-    io.emit('send message', msg);
-  });
+// io.on('connection', (socket) => {
+//   console.log('user connected');
+//   socket.on('send message', msg => {
+//     io.emit('send message', msg);
+//   });
+// });
+
+app.post("/abc", (req, res) => {
+  let data = req.body.test;
+  console.log(data);
+  res.send(data);
 });
 
-server.listen(port, function () {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
